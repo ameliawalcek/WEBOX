743f988ed4fccba041509c98b271c97d6ecc2ca9
MichalSagi <smitzi@gmail.com>
2020-09-01 19:08:20 +0300

    error handeling

--- src/Components/CreatorPage/CreatorPage.jsx
+++ src/Components/CreatorPage/CreatorPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react'
+import Header from '../Header/Header'
 
 const CreatorPage = inject('userStore', 'mediaStore')(observer((props) => {
 
--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -1,32 +1,51 @@
-import { observable, action, computed } from 'mobx'
-import axios from 'axios'
+import { observable, action, computed } from "mobx";
+import axios from "axios";
 
 export class UserStore {
-    @observable favorites = []
-    @observable isLoggedIn = false
-    @observable notifications = []
+  @observable userId = "";
+  @observable favorites = [];
+  @observable isLoggedIn = false;
+  @observable notifications = [];
+  
 
-    @action async getUser(id){
-        let data = await axios.get(`ADDRESS/user/${id}`)
-        //update state with user favorites and notifications
+  @action async getUser(id) {
+    let data = await axios.get(`ADDRESS/user/${id}`);
+  }
+
+  @action async checkUser(user) {
+    const result = await axios.post("/login", user);
+    if (result.status === 202) {
+        this.isLoggedIn = true
+        this.userId = result._id
+        this.getUser(this.userId)
     }
+    alert(result.message);
+  }
 
-    //get favorites (do we need?)
-
-    @action async saveFavorite(id){
-        let favorite = await axios.post(`ADDRESS/user/favorites/${id}`)
-        this.favorites.push(/*update state with favorite*/)
+  @action async saveUser(user) {
+    const result = await axios.post("/signup", user);
+    if (result.status === 201) {
+        this.isLoggedIn = true
+        this.userId = result._id
     }
+    alert(result.message);
+  }
 
-    @action async deleteFavorite(id){
-        let deletedFavorite = await axios.delete(`ADDRESS/user/favorites/${id}`)
-        this.favorites.find(/*delete favorite from state*/)
-    }
+  //get favorites (do we need?)
 
-    //get notifications (do we need?)
+  @action async saveFavorite(id) {
+    let favorite = await axios.post(`ADDRESS/user/favorites/${id}`);
+    this.favorites.push(/*update state with favorite*/);
+  }
 
-    @action async deleteNotification(id){
-        let deletedNotification = await axios.delete(`ADDRESS/user/notifications/${id}`)
-    }
+  @action async deleteFavorite(id) {
+    let deletedFavorite = await axios.delete(`ADDRESS/user/favorites/${id}`);
+    this.favorites.find(/*delete favorite from state*/);
+  }
 
+  //get notifications (do we need?)
+
+  @action async deleteNotification(id) {
+    let deletedNotification = await axios.delete(`ADDRESS/user/notifications/${id}`);
+  }
 }
