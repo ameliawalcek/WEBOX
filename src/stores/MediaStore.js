462d2dcf6c0e167306630ac14f589b51a91620b4
MichalSagi <smitzi@gmail.com>
2020-09-02 13:43:41 +0300

    searchbar

--- src/stores/MediaStore.js
+++ src/stores/MediaStore.js
@@ -3,6 +3,20 @@ import axios from 'axios'
 
 export class MediaStore {
     @observable creators = []
+    @observable trending = []
+    @observable searchResults = []
 
-    
+    @action async getTrending(category) {
+        let trending = await axios.get('/') // 
+        this.trending = trending
+    }
+
+    @action async getCreators() {
+        let creatorsFromDb = await axios.get('/') // 
+        this.creators = creatorsFromDb
+    }
+
+    @action searchCreators(value) {
+        this.searchResults = this.creators.filter(c => c.twitch.toLowerCase().includes(value));
+    }
 }
