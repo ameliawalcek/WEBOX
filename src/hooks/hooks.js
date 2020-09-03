9ce62d6d54024c4c4a57224797470d48698e7d3f
Chen Ozer <chen572@gmail.com>
2020-09-03 13:18:00 +0300

    added useTheme hook and darkMode logic

--- src/hooks/hooks.js
+++ src/hooks/hooks.js
@@ -1,3 +1,5 @@
+import { orange, lightBlue, deepPurple, deepOrange } from "@material-ui/core/colors"
+import { createMuiTheme } from '@material-ui/core'
 import { useEffect } from "react"
 
 export const useScript = url => {
@@ -12,5 +14,31 @@ export const useScript = url => {
     return () => {
       document.body.removeChild(script)
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+}
+
+export const useTheme = darkState => {
+  const palletType = darkState ? "dark" : "light"
+  const mainPrimaryColor = darkState ? orange[500] : lightBlue[500]
+  const mainSecondaryColor = darkState ? deepOrange[900] : deepPurple[500]
+
+  return createMuiTheme({
+    palette: {
+      type: palletType,
+      primary: {
+        main: mainPrimaryColor
+      },
+      secondary: {
+        main: mainSecondaryColor
+      }
+    },
+    overrides: {
+      MuiButton: {
+        text: {
+          background: mainPrimaryColor
+        }
+      }
+    }
+  })
 }
