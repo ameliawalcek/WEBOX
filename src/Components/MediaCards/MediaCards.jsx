45a84619c9964917dbf5a74f4ffe3d20546634fa
Chen Ozer <chen572@gmail.com>
2020-09-03 11:00:42 +0300

    worked on the creator page

--- src/Components/MediaCards/MediaCards.jsx
+++ src/Components/MediaCards/MediaCards.jsx
@@ -1,70 +1,72 @@
-import React from 'react'
-import Header from '../Header/Header'
-import MediaCard from './MediaCard'
-import EmptyCard from './EmptyCard'
-import CategoryBar from './CategoryBar'
-import { inject, observer } from 'mobx-react'
-import { useLocation } from 'react-router-dom'
-import { Grid, GridList, makeStyles } from '@material-ui/core'
+import React from 'react';
+import Header from '../Header/Header';
+import MediaCard from './MediaCard';
+import EmptyCard from './EmptyCard';
+import CategoryBar from './CategoryBar';
+import { inject, observer } from 'mobx-react';
+import { useLocation } from 'react-router-dom';
+import { Grid, GridList, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'flex-start',
-        overflow: 'hidden',
-        backgroundColor: theme.palette.background.paper,
-        width: '100%',
-    },
-    icon: {
-        color: 'rgba(255, 255, 255, 0.54)',
-    },
-    container: {
-        marginTop: 5,
-    },
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'flex-start',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+    width: '100%',
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
+  container: {
+    marginTop: 5,
+  },
 }));
 
-const MediaCards = inject('userStore', 'mediaStore')(observer((props) => {
-    const location = useLocation()
-    const classes = useStyles()
+const MediaCards = inject(
+  'userStore',
+  'mediaStore'
+)(
+  observer((props) => {
+    const location = useLocation();
+    const classes = useStyles();
 
-    let { isLoggedIn, favorites } = props.userStore
-    let { trending, searchResults } = props.mediaStore
+    let { isLoggedIn, favorites } = props.userStore;
+    let { trending, searchResults } = props.mediaStore;
 
     const { media, header, mediaCard } =
-        location.pathname === '/dashboard' && (!isLoggedIn || !favorites.length)
-            ? { media: [], header: 'basic', mediaCard: false }
-            
-            : location.pathname === '/dashboard'
-                ? { media: favorites, header: 'basic', mediaCard: true }
-                
-                : { media: trending, header: 'explore', mediaCard: true }
+      location.pathname === '/dashboard' && (!isLoggedIn || !favorites.length)
+        ? { media: [], header: 'basic', mediaCard: false }
+        : location.pathname === '/dashboard'
+        ? { media: favorites, header: 'basic', mediaCard: true }
+        : { media: trending, header: 'explore', mediaCard: true };
 
     const renderMediaCard = (data) => {
-        return data.map((d) => {
-            return (
-                <MediaCard id={d._id} img={d.img} twitchName={d.twitch} key={d._id} />
-            )
-        })
-    }
+      return data.map((d) => {
+        return (
+          <MediaCard id={d._id} img={d.img} twitchName={d.twitch} key={d._id} />
+        );
+      });
+    };
 
     return (
-        <>
-            <Header page={header} />
-            {header === 'explore' && <CategoryBar />}
-            {mediaCard
-                ? <Grid container className={header === 'explore' && classes.container}>
-                    <GridList cellHeight={180} className={classes.root}>
-                        {searchResults.length
-                            ? renderMediaCard(searchResults)
-                            : renderMediaCard(media)}
-                    </GridList>
-                </Grid>
-
-                : <EmptyCard />
-            }
-        </>
-    )
-})
-)
-export default MediaCards
+      <>
+        <Header page={header} />
+        {header === 'explore' && <CategoryBar />}
+        {mediaCard ? (
+          <Grid container className={header === 'explore' && classes.container}>
+            <GridList cellHeight={180} className={classes.root}>
+              {searchResults.length
+                ? renderMediaCard(searchResults)
+                : renderMediaCard(media)}
+            </GridList>
+          </Grid>
+        ) : (
+          <EmptyCard />
+        )}
+      </>
+    );
+  })
+);
+export default MediaCards;
