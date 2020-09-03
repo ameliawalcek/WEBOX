8112e0b14f8aa9c45213141d21c955ad50d4a9cb
ameliawalcek <ameliawalcek@gmail.com>
2020-09-04 00:19:30 +0300

    style

--- src/hooks/hooks.js
+++ src/hooks/hooks.js
@@ -1,28 +1,27 @@
-import { orange, lightBlue, deepPurple, deepOrange } from "@material-ui/core/colors"
 import { createMuiTheme } from '@material-ui/core'
 import { useEffect } from "react"
-
+ 
 export const useScript = url => {
   const script = document.createElement('script')
-
+ 
   script.src = url
   script.async = true
-
+ 
   document.body.appendChild(script)
-
+ 
   useEffect(() => {
     return () => {
       document.body.removeChild(script)
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 }
-
+ 
 export const useTheme = darkState => {
   const palletType = darkState ? "dark" : "light"
-  const mainPrimaryColor = darkState ? orange[500] : lightBlue[500]
-  const mainSecondaryColor = darkState ? deepOrange[900] : deepPurple[500]
-
+  const mainPrimaryColor = darkState ? '#303030' : '#673ab7'
+  const mainSecondaryColor = darkState ? '#954bb4' : '#009688'
+ 
   return createMuiTheme({
     palette: {
       type: palletType,
