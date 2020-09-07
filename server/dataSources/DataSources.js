239646f2ffa00c2273a20ad8b7fad3b016a9b824
Chen Ozer <chen572@gmail.com>
2020-09-07 17:10:46 +0300

    get creator test

--- server/dataSources/DataSources.js
+++ server/dataSources/DataSources.js
@@ -13,10 +13,8 @@ class DataSources {
 
   async getCreatorLinksByid(id) {
     const creator = await this.mongoClient.getCreatorById(id)
-    const [instagramPostId, youtubeVideoId] = await Promise.all([
-      this.instagramAPI.getRecentPostByRef(creator.instagram),
-      this.youtubeAPI.getYoutubeLatestByRef(creator.youtube)  
-    ]).catch(e => console.log(e))
+    const instagramPostId = this.instagramAPI.getRecentPostByRef(creator.instagram).then(d => d).catch(e => console.log(e))
+    const youtubeVideoId = this.youtubeAPI.getYoutubeLatestByRef(creator.youtube).then(d => d).catch(e => console.log(e))
     return {
       imgUrl: creator.img,
       twitchName: creator.twitch,
