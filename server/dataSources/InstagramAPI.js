e90511fbbb8c8438a425c625e219c274a2db925f
Chen Ozer <chen572@gmail.com>
2020-09-10 13:51:47 +0300

    added logging for instagram

--- server/dataSources/InstagramAPI.js
+++ server/dataSources/InstagramAPI.js
@@ -8,6 +8,7 @@ class InstagramAPI {
   async getRecentPostByRef(ref) {
     try {
       const instagramGql = await axios(this.baseUrl + ref + '?__a=1')
+      console.log(instagramGql.data)
       if (instagramGql.data) {
         return instagramGql.data.graphql.user.edge_owner_to_timeline_media.edges[0].node.shortcode
       }
